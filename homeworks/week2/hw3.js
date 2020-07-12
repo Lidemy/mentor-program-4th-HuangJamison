95cd2c7a3310aa893c7cbc1550c34cfa3193624d
J米 <b10130402@gmail.com>
2020-07-12 20:38:04 +0800

    week2 作業含一題挑戰題

--- homeworks/week2/hw3.js
+++ homeworks/week2/hw3.js
@@ -1,5 +1,9 @@
 function reverse(str) {
-  
+    var res = '';
+    for (let i=str.length-1; i>=0; i--) {
+        res += str[i];
+    }
+    console.log(res);
 }
 
 reverse('hello');
--- homeworks/week2/hw5.js
+++ homeworks/week2/hw5.js
@@ -1,10 +1,30 @@
 function join(arr, concatStr) {
-  
+    // 想法為把陣列裡的東西拿出來，如果陣列為最後一個數則不串連
+    if (arr.length == 0) {
+        return;
+    }
+    var res = '';
+    for (let i=0; i<=arr.length-1; i++) {
+        if (i == arr.length-1) {
+            res += arr[i];
+            return res;
+        }
+        res += arr[i];
+        res += concatStr;
+    }
 }
 
 function repeat(str, times) {
-  
+    // times 作為 for 內 i 的次數
+    var res = '';
+    if (Number(times) < 1) {
+        return;
+    }
+    for (let i=1; i<=times; i++) {
+        res += str;
+    }
+    return res;
 }
 
-console.log(join(['a'], '!'));
-console.log(repeat('a', 5));
+console.log(join(["aaa", "bb", "c", "dddd"], ',,'));
+console.log(repeat('jamie', 5));
